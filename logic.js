93dbeb577455f18ed00fff524c8dac9882568f60
Richard Perko <richardperko@outlook.com>
2022-01-31 11:19:33 -0700

    logic finished next is css!

--- logic.js
+++ logic.js
@@ -1,5 +1,15 @@
 //game function will start the process of playing the game
 
+//needed variables to be able to run the game
+var playerWin;
+var computerWin;
+var drawRound;
+
+let playerScore = 0;
+let computerScore = 0;
+let draws = 0;
+
+//selectors that allow us to click the buttons and return the results
 const rock = document.querySelector('#rock');
 rock.onclick = () => game("rock");
 
@@ -9,44 +19,47 @@ paper.onclick = () => game("paper");
 const scissors = document.querySelector('#scissors');
 scissors.onclick = () => game("scissors");
 
+//dom manipulations to print the results of the game
 const displayResults = document.querySelector('#displayResults')
-const results = document.createElement('div')
+const results = document.createElement('div');
 results.classList.add('results');
-
 displayResults.append(results);
-var playerWin;
-var computerWin;
-var drawRound;
 
-let playerScore = 0;
-let computerScore = 0;
-let draws = 0;
+const buttons = document.querySelector('#buttonContainer');
+const scoreKeeper = document.createElement('div');
+scoreKeeper.classList.add('scoreKeeper');
+buttons.append(scoreKeeper);
 
+//main function
 function game(playerChoice){
+   // gathers the information to play the game, players choice is passed in
+   // through the button click, while computer choice calls a function
     let playerSelection = playerChoice;
     let computerSelection = computerPlay()
-
+   //calls the play function that compares the two selections
     let result = playRound(playerSelection, computerSelection)
+    // if statements that will keep track of the score and print the score totals after each round.
     if (result === "playerWin"){
        playerScore++;
-       console.log("player: ",playerScore)
+       scoreKeeper.textContent = "Player: " + playerScore + " Computer: " + computerScore ;
     } else if (result === "drawRound") {
        draws++;
-       console.log("draws", draws)
+       scoreKeeper.textContent= "Player: " + playerScore + " Computer: " + computerScore ;
     } else {
        computerScore++;
-       console.log("computer: ", computerScore)
+       scoreKeeper.textContent= "Player: " + playerScore + " Computer: " + computerScore ;
     }
-    
+    // scorekeeper that determines the winner of the game and prints the message
     if (playerScore === 5){
-       console.log("Player wins!")
+       results.textContent= "Player wins the game!"
        resetScore();
     } else if (computerScore === 5){
-       console.log("Computer Wins")
+       results.textContent = "Computer wins the game!"
        resetScore();
     }
 }
 
+//resets the score for a new game
 function resetScore(){
    playerScore = 0;
    computerScore = 0;
@@ -59,15 +72,12 @@ function computerPlay(){
    let computerResult = Math.floor(Math.random() * 3) + 1
    switch (computerResult){
        case 1:
-           console.log("Computers choice: Rock")
            return "rock"
            break;
        case 2:
-           console.log("Computers choice: Paper")
            return "paper"
            break;
        case 3:
-           console.log("Computers choice: Scissors")
            return "scissors"
            break;
    }
