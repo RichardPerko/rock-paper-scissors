63a5be8645ba31283914f948101d0ced1c242df9
Richard Perko <richardperko@outlook.com>
2022-01-31 10:48:58 -0700

    logic is working now, time to add event handlers

--- logic.js
+++ logic.js
@@ -14,14 +14,43 @@ const results = document.createElement('div')
 results.classList.add('results');
 
 displayResults.append(results);
+var playerWin;
+var computerWin;
+var drawRound;
 
+let playerScore = 0;
+let computerScore = 0;
+let draws = 0;
 
 function game(playerChoice){
     let playerSelection = playerChoice;
     let computerSelection = computerPlay()
 
-    console.log (playRound(playerSelection, computerSelection))
-  
+    let result = playRound(playerSelection, computerSelection)
+    if (result === "playerWin"){
+       playerScore++;
+       console.log("player: ",playerScore)
+    } else if (result === "drawRound") {
+       draws++;
+       console.log("draws", draws)
+    } else {
+       computerScore++;
+       console.log("computer: ", computerScore)
+    }
+    
+    if (playerScore === 5){
+       console.log("Player wins!")
+       resetScore();
+    } else if (computerScore === 5){
+       console.log("Computer Wins")
+       resetScore();
+    }
+}
+
+function resetScore(){
+   playerScore = 0;
+   computerScore = 0;
+   draws = 0;
 }
 
 //gets the result of the computer by randomly generating a number
@@ -49,17 +78,24 @@ function computerPlay(){
 function playRound(playerSelection, computerSelection){
  if (playerSelection === computerSelection){
      results.textContent = "Draw!"
+     return "drawRound";
  } else if (playerSelection === "rock" && computerSelection === "scissors"){
     results.textContent = "Player wins! Rock beats Scissors!"
+    return "playerWin"
  } else if (playerSelection === "scissors" && computerSelection === "paper"){
     results.textContent = "Player wins! Scissors beats paper!"
+    return "playerWin"
  } else if (playerSelection === "paper" && computerSelection === "rock"){
     results.textContent = "Player wins! Paper beats Rock!"
+    return "playerWin"
  } else if (computerSelection === "rock" && playerSelection === "scissors"){
     results.textContent = "Computer wins! Rock beats Scissors!"
+    return "computerWin"
  } else if (computerSelection === "scissors" && playerSelection === "paper"){
     results.textContent = "Computer wins! Scissors beats paper!"
+    return "computerWin"
  }  else if (computerSelection === "paper" && playerSelection === "rock"){
     results.textContent = "Computer wins! Paper beats Rock!"
+    return "computerWin"
  }
 }
